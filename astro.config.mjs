851ac29646947adbe58070a2f041f0ac7d49762c
Daniel Barrera <daniel.barrera02121@hotmail.com>
2026-06-08 13:00:15 -0500

    add seo performance

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  integrations: [tailwind()],
+  site: 'https://horsepayo.com',
+  integrations: [
+    tailwind(),
+  ],
 });
